380b8326caf0c9ee4a19b8285fbd289c113c2760
nerokome <nerooghenekome@gmail.com>
2025-06-19 17:00:11 +0100

    end

--- my-react-app/src/components/End.jsx
+++ my-react-app/src/components/End.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const End = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-[100px] bg-gray-200 text-gray-700'>
-      <p className='font-mono italic'>Wagmi @2025</p>
+    <div className='flex flex-col items-center justify-center h-[50px] bg-gray-400 text-gray-700'>
+      <p className='font-mono italic shadow-gray-800 shadow-2xl'>Wagmi @2025</p>
     </div>
   )
 }
